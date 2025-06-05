5601215481899d8dcd58e4f695c436d16ef7b8a7
Kacper Arendt <71129131+Kacper-Arendt@users.noreply.github.com>
2025-06-05 21:32:53 +0200

    list

--- apps/web/src/features/budget/components/categories/Categories.tsx
+++ apps/web/src/features/budget/components/categories/Categories.tsx
@@ -1,3 +1,20 @@
-export const Categories = ({ budgetId }: { budgetId: string }) => {
-	return <>{budgetId}</>;
+import { getCategories } from "@/features/budget/api/categories";
+import { CategoriesList } from "@/features/budget/components/categories/CategoriesList";
+import { getTranslations } from "next-intl/server";
+
+export const Categories = async ({ budgetId }: { budgetId: string }) => {
+	const t = await getTranslations("budget");
+	const categories = await getCategories(budgetId);
+
+	const expenses = categories?.filter((cat) => cat.type === 1);
+	const incomes = categories?.filter((cat) => cat.type === 2);
+
+	return (
+		<>
+			<div className="flex flex-wrap gap-20">
+				<CategoriesList title={t("expenses")} list={expenses ?? []} />
+				<CategoriesList title={t("incomes")} list={incomes ?? []} />
+			</div>
+		</>
+	);
 };
