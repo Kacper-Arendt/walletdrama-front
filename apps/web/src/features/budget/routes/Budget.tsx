c9e285ec81d0bdf54e8b0796c188b79370ba173c
Kacper Arendt <71129131+Kacper-Arendt@users.noreply.github.com>
2025-06-05 20:44:30 +0200

    add categories tempalte

--- apps/web/src/features/budget/routes/Budget.tsx
+++ apps/web/src/features/budget/routes/Budget.tsx
@@ -2,13 +2,14 @@ import { getBudget } from "@/features/budget/api/getBudget";
 import { BudgetDetails } from "@/features/budget/components/BudgetDetails";
 import { BudgetHeader } from "@/features/budget/components/BudgetHeader";
 import { BudgetSettings } from "@/features/budget/components/BudgetSettings";
+import { Categories } from "@/features/budget/components/categories/Categories";
 import {
 	Tabs,
 	TabsContent,
 	TabsList,
 	TabsTrigger,
 } from "@ui/components/ui/tabs";
-import { FileText, Settings } from "lucide-react";
+import { ChartBarStacked, FileText, Settings } from "lucide-react";
 import { getTranslations } from "next-intl/server";
 import { notFound } from "next/navigation";
 
@@ -37,6 +38,10 @@ export const Budget = async ({ id }: { id: string }) => {
 					{/*	<Calendar className="mr-2 h-4 w-4" />*/}
 					{/*	Months*/}
 					{/*</TabsTrigger>*/}
+					<TabsTrigger value="categories">
+						<ChartBarStacked className="mr-2 h-4 w-4" />
+						{t("categories")}
+					</TabsTrigger>
 					<TabsTrigger value="settings">
 						<Settings className="mr-2 h-4 w-4" />
 						{t("settings")}
@@ -45,6 +50,10 @@ export const Budget = async ({ id }: { id: string }) => {
 
 				<TabsContent value="details" className="space-y-4 pt-4">
 					<BudgetDetails id={id} />
+				</TabsContent>
+
+				<TabsContent value="categories" className="space-y-4 pt-4">
+					<Categories budgetId={id} />
 				</TabsContent>
 
 				{/*<TabsContent value="overview" className="space-y-4 pt-4">*/}
